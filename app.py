14a6468e45392e973270c9fa015fac76d4804e38
LucasPS23 <lucasan2011@gmail.com>
2025-06-06 12:20:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from flask import Flask, request
 from controller import connect_db, pesquisar_dados
 
@@ -10,17 +10,4 @@ app = Flask(__name__)
 
 
 if __name__=='__name__':
-=======
-from flask import Flask, request
-from controller import connect_db, pesquisar_dados
-
-
-app = Flask(__name__)
-
-
-
-
-
-if __name__=='__name__':
->>>>>>> 4665025c03959af20def22a9d033fea9eac0f70d
     app.run(debug=True)
