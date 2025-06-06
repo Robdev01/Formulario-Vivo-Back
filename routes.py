caf74bfa02790427cdb701e13878a257fb7cc499
LucasPS23 <lucasan2011@gmail.com>
2025-06-06 12:19:33 -0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, request
 from controller import pesquisar_dados
 
@@ -23,33 +22,3 @@ def pesquisa():
 @app.route('/admin')
 def admin():
    
-
-
-=======
-from flask import Flask, request
-from controller import pesquisar_dados
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-   
-
-@app.route('/login')
-def login():
-   
-
-@app.route('/pesquisa',methods=['GET', 'POST'])
-def pesquisa():
-    if request.method == 'POST':
-        query = request.form['campo_de_pesquisa']
-        resultados = pesquisar_dados(query)
-   
-
-
-@app.route('/admin')
-def admin():
-   
-
-
->>>>>>> 4665025c03959af20def22a9d033fea9eac0f70d
